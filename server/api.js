d2186eb391524f4fd072ddd52ce62e4414abb6cf
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-10-05 12:19:02 +0100

    edding skills

--- server/api.js
+++ server/api.js
@@ -21,7 +21,8 @@ router.get("/", (_, res) => {
 
 router.post("/signup", (req, res) => {
 	console.log("register api");
-	const { firstname,lastname, email, password,cohort,usertype } = req.body;
+	const { firstname, lastname, email, password, cohort, usertype, html_css,javascript,react,node,postgresql,mongodb } =
+		req.body;
 	const isValidEmail = EmailValidator.validate(email);
 	const schema = new passwordValidator();
 	schema
@@ -44,15 +45,21 @@ router.post("/signup", (req, res) => {
 	const saltedPassword = SHA256(
 		hashedPassword + process.env.PASSWORD_SALT
 	).toString();
-	if (isValidEmail && isValidPassword && firstname && lastname && cohort && usertype) {
+	if (isValidEmail && isValidPassword && firstname && lastname && usertype) {
 		const newUser = {
 			id: uuid.v4(),
 			firstname: firstname,
-			lastname:lastname,
+			lastname: lastname,
 			email: email,
 			password: saltedPassword,
-			cohort:cohort,
-			usertype:usertype,
+			cohort: cohort,
+			usertype: usertype,
+			html_css: html_css,
+			javascript: javascript,
+			react: react,
+			node:node,
+			postgresql:postgresql,
+			mongodb:mongodb,
 		};
 		pool.query(
 			"SELECT * FROM users where email=$1",
@@ -62,7 +69,7 @@ router.post("/signup", (req, res) => {
 					res.json({ register: "error-registereduser" });
 				} else {
 					pool.query(
-						"INSERT INTO users (id,firstname,lastname,email,password,cohort,user_type) VALUES ($1,$2,$3,$4,$5,$6,$7)",
+						"INSERT INTO users (id,firstname,lastname,email,password,cohort,user_type,html_css,javascript,react,node,postgresql,mongodb) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)",
 						[
 							newUser.id,
 							newUser.firstname,
@@ -71,6 +78,12 @@ router.post("/signup", (req, res) => {
 							newUser.password,
 							newUser.cohort,
 							newUser.usertype,
+							newUser.html_css,
+							newUser.javascript,
+							newUser.react,
+							newUser.node,
+							newUser.postgresql,
+							newUser.mongodb,
 						],
 						(error, result) => {
 							res.json({ register: "success" });
@@ -133,7 +146,7 @@ router.post("/signin", (req, res) => {
 
 //verify token middleware
 function authenticateToken(req, res, next) {
-	console.log("authenticate function called")
+	console.log("authenticate function called");
 	const authHeader = req.headers["authorization"];
 	console.log("AUTHEADER IS____"+authHeader);
 	const token = authHeader && authHeader.split(" ")[1];
