7c9e1fa25239fe6546c1dd8d64bed1819b513a05
yunusfirat <yunusfirat44@gmail.com>
2021-10-08 03:46:50 +0100

    debug

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { FaArrowUp } from "react-icons/fa";
 export default function Footer() {
-    return (
-        <footer className="p-5 bg-dark text-white text-center position-relative fixed-bottom">
-        <div className="container">
-          <p className="lead">Copyright &copy; 2021 Hackathon Team-2</p>
-          <Link to="/" className="position-absolute bottom-0 end-0 p-5"><FaArrowUp  size="3rem" /></Link>
-        </div>
-      </footer>
-    );
+  return (
+    <footer className="p-5 bg-dark text-white text-center position-relative fixed-bottom">
+      <div className="container">
+        <p className="lead">Copyright &copy; 2021 Hackathon Team-2</p>
+        {/* <Link to="/" ></Link> */}
+        <div className="position-absolute bottom-0 end-0 p-5"><FaArrowUp  size="3rem"  color="" /></div>
+      </div>
+    </footer>
+  );
 }
