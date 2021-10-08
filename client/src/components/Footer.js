fbe71edb1ae612d686c219874121d6664e5e7e2a
yunusfirat <yunusfirat44@gmail.com>
2021-10-08 04:38:32 +0100

    debug

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     window.scrollTo(0, 0);
   };
   return (
-    <footer className="p-5 bg-dark text-white text-center position-relative fixed-bottom border ">
+    <footer className="p-5 bg-dark text-white text-center position-relative fixed-bottom">
       <div className="container">
         <p className="lead">Copyright &copy; 2021 Hackathon Team-2</p>
         <button onClick={buttonClick} className="btn btn-dark mb-3 position-absolute bottom-0 end-0 "><FaArrowUp  size="2rem"  color="" /></button>
