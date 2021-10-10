e1272c30a40fd1a8a3256093dcd10a059bbccfbe
Dominika <domi_nika@hotmail.co.uk>
2021-10-10 20:57:43 +0100

    creat cohort / session buttons

--- client/src/pages/AdminDashboard/index.js
+++ client/src/pages/AdminDashboard/index.js
@@ -1,15 +1,25 @@
 import AttendanceGraph from "../../components/AttendanceGraph";
 import UpcomingSessionsSidebar from "../../components/UpcomingSessionsSidebar";
-
+import Navbarcomponent from "../../components/Navbarcomponent";
 import "./AdminDashboard.css";
+import Button from "../../components/Button";
 
 const AdminDashboard = () => {
-return(
-  <div className="admin-main-container">
-    <UpcomingSessionsSidebar />
-    <AttendanceGraph />
-  </div>
-);
+	const name = "Sarah";
+	return (
+		<>
+			<Navbarcomponent />
+			<h1 className="white-text center-text"> Welcome {name}!</h1>
+			<div className="button-container">
+				<Button label="New session" mode="primary" size="medium" />
+				<Button label="New cohort" mode="secondary" size="medium" />
+			</div>
+			<div className="admin-main-container">
+				<UpcomingSessionsSidebar />
+				<AttendanceGraph />
+			</div>
+		</>
+	);
 };
 
 export default AdminDashboard;
