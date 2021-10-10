29eab08fe0c7e075b48d232efbc2aef62d9f82e1
Dominika <domi_nika@hotmail.co.uk>
2021-10-10 20:56:20 +0100

    added filter bar, updated default text colour

--- client/src/components/AttendanceGraph/index.js
+++ client/src/components/AttendanceGraph/index.js
@@ -1,26 +1,53 @@
+import { useState } from "react";
 import { Bar } from "react-chartjs-2";
+import { defaults } from "react-chartjs-2";
+import AttendanceDateFilters from "../AttendanceDateFilters";
 
 import "./AttendanceGraph.css";
 
 const AttendanceGraph = () => {
-  const state = {
-    labels: ["January", "February", "March",
-             "April", "May"],
-    datasets: [
-      {
-        label: "Rainfall",
-        backgroundColor: "rgba(75,192,192,1)",
-        borderColor: "rgba(0,0,0,1)",
-        borderWidth: 2,
-        data: [65, 59, 80, 81, 56],
-      },
-    ],
-  };
+	const currentMonth = "September";
+	const currentYear = 2023;
 
-  return (
-    <div className="graph-container">
-      <Bar data={state} options={{ title:{ display:true, text:"Average Rainfall per month",fontSize:20 }, legend:{ display:true, position:"right" } }} />
-    </div>
-  );
+	const [month, setMonth] = useState(currentMonth);
+	const [year, setYear] = useState(currentYear);
+
+	defaults.color = "#f8f9fa";
+	const options = {
+		scales: {
+			yAxes: [
+				{
+					ticks: {
+						beginAtZero: true,
+					},
+				},
+			],
+		},
+	};
+
+	const state = {
+		labels: ["January", "February", "March", "April", "May"],
+		datasets: [
+			{
+				backgroundColor: "#ed4343",
+				data: [65, 59, 80, 81, 56],
+				label: "Attendance",
+			},
+		],
+	};
+
+	return (
+		<>
+			<div className="graph-container white-text">
+				<AttendanceDateFilters
+					month={month}
+					setMonth={setMonth}
+					year={year}
+					setYear={setYear}
+				/>
+				<Bar data={state} options={options} />
+			</div>
+		</>
+	);
 };
 export default AttendanceGraph;
