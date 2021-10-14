49c7350991083da341f9bb8848317565825df4f4
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-10-14 12:07:28 +0100

    delete account endpoint

--- server/api.js
+++ server/api.js
@@ -354,5 +354,27 @@ router.post("/assignadmin", authenticateToken, (req, res) => {
 		});
 });
 
+router.post("/deleteaccount", authenticateToken, (req, res) => {
+	const { email } = req.body;
+	const userID = req.user.userid;
+	pool
+		.query("SELECT user_type from users where id=$1;", [userID])
+		.then((result) => {
+			let user_type = result.rows[0].user_type;
+			if (user_type === "admin") {
+				pool.query("SELECT email from users WHERE email=$1;", [email]).then((result)=>{
+                if (result.rows.length>0) {
+                pool.query("DELETE from users WHERE email=$1;", [email]);
+				res.sendStatus(200);
+                } else{
+				res.sendStatus(400);
+				}
+				});
+			} else {
+				res.sendStatus(401);
+			}
+		});
+});
+
 export default router;
 
