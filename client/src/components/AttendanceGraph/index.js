4a9caf4645de32b76cf5ad4f2ad0a08bbc2e3a51
Dominika <domi_nika@hotmail.co.uk>
2021-10-16 12:49:34 +0100

    attendance graph wip

--- client/src/components/AttendanceGraph/index.js
+++ client/src/components/AttendanceGraph/index.js
@@ -1,40 +1,16 @@
-import { useState } from "react";
 import { Bar } from "react-chartjs-2";
-import { defaults } from "react-chartjs-2";
-import AttendanceDateFilters from "../AttendanceDateFilters";
+import {
+	attendanceGraphDefaults,
+	options,
+} from "../../constants/attendanceGraphConfig";
 
 import "./AttendanceGraph.css";
 
-const AttendanceGraph = ({ type }) => {
-	defaults.color = "#f8f9fa";
-	const options = {
-		scales: {
-			yAxes: [
-				{
-					ticks: {
-						beginAtZero: true,
-					},
-				},
-			],
-		},
-	};
-
-	const state = {
-		labels: ["January", "February", "March", "April", "May"],
-		datasets: [
-			{
-				backgroundColor: "#ed4343",
-				data: [65, 59, 80, 81, 56],
-				label: "Attendance",
-			},
-		],
-	};
-
+const AttendanceGraph = () => {
 	return (
 		<>
 			<div className="graph-container">
-				<h3>{type} attendance </h3>
-				<Bar data={state} options={options} />
+				<Bar data={attendanceGraphDefaults} options={options} />
 			</div>
 		</>
 	);
