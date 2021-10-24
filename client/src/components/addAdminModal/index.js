20e8178e66e604c844b10d167f5f55989f4763e2
Dominika <domi_nika@hotmail.co.uk>
2021-10-24 22:46:44 +0100

    modal for adding new admin

---
+++ client/src/components/addAdminModal/index.js
@@ -0,0 +1,32 @@
+import { Modal } from "react-bootstrap";
+import Button from "../Button";
+import { useRef } from "react";
+
+const AddAdminModal = ({ show, setShowModal }) => {
+	const handleClose = () => setShowModal(false);
+	const adminEmail = useRef();
+
+	const addAdmin = () => {
+		console.log(adminEmail.current.value);
+	};
+
+	return (
+		<Modal show={show} onHide={handleClose}>
+			<Modal.Header closeButton>
+				<Modal.Title>Add a new admin</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<p> Enter the email address of the user you'd like to make an admin.</p>
+				<input ref={adminEmail}></input>
+				<Button
+					label="Add Admin"
+					onClick={addAdmin}
+					size="medium"
+					mode="primary"
+				/>
+			</Modal.Body>
+		</Modal>
+	);
+};
+
+export default AddAdminModal;
