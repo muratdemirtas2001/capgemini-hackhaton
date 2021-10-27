9e40a97b1199b6f56228d70ebaf69e53c68aac4f
Dominika <domi_nika@hotmail.co.uk>
2021-10-27 20:20:10 +0100

    logic for seesion details and request mentors modals

--- client/src/utils/displaySessionsForAdmin.js
+++ client/src/utils/displaySessionsForAdmin.js
@@ -1,43 +1,109 @@
+import { useState } from "react";
+import { Modal } from "react-bootstrap";
+
 import Button from "../components/Button";
+import ResponsiveTable from "../components/ResponsiveTable";
+import mentorsTableHeadings from "../constants/mentorsTableHeadings";
+import mentorsData from "../data/mentors";
 import checkStudentVolunteerRatio from "./checkStudentVolunteerRatio";
+import displayMentorsTableForSession from "./displayMentorsTableForSession";
 
 const displayUpcomingSessionsForAdmin = (upcomingSessions) => {
 	return upcomingSessions.map((session, index) => {
+		const [show, setShow] = useState();
+		const [modalContent, setModalContent] = useState();
+		const handleClose = () => setShow(false);
+		const id = session.session_id;
+
+		const cancelSession = () => {
+			console.log(id);
+		};
+
+		const getSessionDetails = () => {
+			//make request to get details of session
+			setModalContent("Details");
+			setShow(true);
+		};
+
+		const getVolunteers = () => {
+			setModalContent(
+				<ResponsiveTable
+					data={mentorsData}
+					displayFunction={displayMentorsTableForSession}
+					headings={["Name", "Skills", "Contact"]}
+				/>
+			);
+			setShow(true);
+		};
+
 		const isRatioAcceptable = checkStudentVolunteerRatio(
 			session.registered_student,
 			session.registered_mentor
 		);
 		return (
-			<tr
-				className={[
-					`isRatioAcceptable--${isRatioAcceptable}`,
-					"upcoming-session-container",
-				].join(" ")}
-				key={`${index}-table-row`}
-			>
-				<td key={`${index}-title`}> {session.session_title}</td>
-				<td key={`${index}-date`}>
-					{session.session_date} {session.start_time} - {session.end_time}
-				</td>
-				<td key={`${index}-ratio`}>
-					{" "}
-					{session.registered_student} : {session.registered_mentor}
-				</td>
-				<td key={`${index}-actions`} id={session.session_id}>
-					{isRatioAcceptable ? (
-						<div>
-							<Button label="Details" size="small" mode="secondary" />
-							<Button label="Cancel" size="small" mode="secondary" />
-						</div>
-					) : (
-						<div>
-							<Button label="Request Volunteers" size="small" mode="primary" />
-							<Button label="Details" size="small" mode="secondary" />
-							<Button label="Cancel" size="small" mode="secondary" />
-						</div>
-					)}
-				</td>
-			</tr>
+			<>
+				<tr
+					className={[
+						`isRatioAcceptable--${isRatioAcceptable}`,
+						"upcoming-session-container",
+					].join(" ")}
+					key={`${index}-table-row`}
+				>
+					<td key={`${index}-title`}> {session.session_title}</td>
+					<td key={`${index}-date`}>
+						{session.session_date} {session.start_time} - {session.end_time}
+					</td>
+					<td key={`${index}-ratio`}>
+						{" "}
+						{session.registered_student} : {session.registered_mentor}
+					</td>
+					<td key={`${index}-actions`}>
+						{isRatioAcceptable ? (
+							<div>
+								<Button
+									label="Details"
+									size="small"
+									mode="secondary"
+									onClick={getSessionDetails}
+								/>
+								<Button
+									label="Cancel"
+									size="small"
+									mode="secondary"
+									onClick={cancelSession}
+								/>
+							</div>
+						) : (
+							<div>
+								<Button
+									label="Request Volunteers"
+									size="small"
+									mode="primary"
+									onClick={getVolunteers}
+								/>
+								<Button
+									label="Details"
+									size="small"
+									mode="secondary"
+									onClick={getSessionDetails}
+								/>
+								<Button
+									label="Cancel"
+									size="small"
+									mode="secondary"
+									onClick={cancelSession}
+								/>
+							</div>
+						)}
+					</td>
+				</tr>
+				<Modal show={show} onHide={handleClose} key={`${index}-details-modal`}>
+					<Modal.Header closeButton>
+						<Modal.Title>Session details</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>{modalContent}</Modal.Body>
+				</Modal>
+			</>
 		);
 	});
 };
