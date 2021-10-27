36ae7dcfc665da8099cc0cffa868c12112e029b0
Dominika <domi_nika@hotmail.co.uk>
2021-10-27 20:19:38 +0100

    display mentors for a session

---
+++ client/src/utils/displayMentorsTableForSession.js
@@ -0,0 +1,26 @@
+import Button from "../components/Button";
+
+const displayMentorsTableForSession = (mentors) => {
+	return mentors.map((mentor, index) => {
+		const requestMentor = () => {
+			console.log(mentor.email);
+		};
+
+		return (
+			<tr key={`${index}-mentors-to-request`}>
+				<td key={`${index}-mentor--name-r`}>{mentor.mentor_name}</td>
+				<td key={`${index}-mentor-skills-r`}>{mentor.skills.join(", ")}</td>
+				<td key={`${index}-mentor-request-btn`}>
+					<Button
+						label="Request"
+						mode="primary"
+						size="small"
+						onClick={requestMentor}
+					/>
+				</td>
+			</tr>
+		);
+	});
+};
+
+export default displayMentorsTableForSession;
